703e24cbb167631680f401919dec9cc68318fa99
TSLCode <scarlettfisherwolf@gmail.com>
2025-10-25 19:03:32 +0000

    Intial Setup of the a

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './styles/App.css';
+import Header from './components/Header';
+
+const App = () => {
+    return (
+        <div className="App">
+            <Header />
+            <h1>Welcome to Pomo</h1>
+        </div>
+    );
+};
+
+export default App;
